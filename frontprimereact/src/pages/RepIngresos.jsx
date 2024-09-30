70857a4dff478146d00d33d22bdf0e473b05f780
Cesar Osorio <cesarozorioa@hotmail.com>
2024-09-30 18:36:59 -0500

    Reporte_Ingresos_V2

--- frontprimereact/src/pages/RepIngresos.jsx
+++ frontprimereact/src/pages/RepIngresos.jsx
@@ -1,8 +1,9 @@
-import  { useState, useEffect } from 'react';
+import   { useState, useEffect, useRef } from 'react';
 import { Calendar } from 'primereact/calendar';
 import { Button } from 'primereact/button';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
+import {Toast} from 'primereact/toast';
 import axios from 'axios';
 
 const RepIngresos = () => {
@@ -11,13 +12,14 @@ const RepIngresos = () => {
     const [ingresos, setIngresos] = useState([]);
     const [productos, setProductos] = useState({});
     const [tiposProducto, setTiposProducto] = useState({});
+    const toast = useRef(null);
 
     useEffect(() => {
         // Cargar productos y tipos de producto al montar el componente
         const fetchProductos = async () => {
+
             const responseProd = await axios.get('http://127.0.0.1:8000/api/v1/producto/');
-            const responseTipoProd = await axios.get('http://127.0.0.1:8000/api/v1/tipo_producto/');
-            
+            const responseTipoProd = await axios.get('http://127.0.0.1:8000/api/v1/tipo_producto/');            
             const prodMap = {};
             const tipoProdMap = {};
 
@@ -37,6 +39,13 @@ const RepIngresos = () => {
     }, []);
 
     const fetchIngresos = async () => {
+        
+        
+        if (!fechaInicial || !fechaFinal) {
+            alert('Por favor, seleccione un rango de fechas');
+            toast.current.show({severity: 'warn', summary: 'Advertencia', detail: 'Por favor, seleccione un rango de fechas'});
+            return;
+        }
         if (fechaInicial && fechaFinal) {
             try {
                 const response = await axios.get('http://127.0.0.1:8000/api/v1/ingreso/', {
@@ -45,15 +54,25 @@ const RepIngresos = () => {
                         fechaFinal: fechaFinal.toISOString().split('T')[0]
                     }
                 });
-                setIngresos(response.data);
+                //Filtrar los ingresos que estan en el rango de fechas
+                const ingresosFiltrados = response.data.filter((ingreso) => {
+                    const fechaIngreso = new Date(ingreso.fechaIngreso);
+                    return fechaIngreso >= fechaInicial && fechaIngreso <= fechaFinal;
+                });
+                if (ingresosFiltrados.length === 0) {
+                    toast.current.show({severity: 'warn', summary: 'Advertencia', detail: 'No se encontraron resultados'});
+                }
+
+                setIngresos(ingresosFiltrados);
             } catch (error) {
                 console.error('Error fetching data', error);
+                toast.current.show({severity: 'error', summary: 'Error', detail: 'Error al obtener datos. Por favor, intente de nuevo.'});
             }
         }
+       
+       
     };
-    console.log("ingresos:>>> ", ingresos);
-    console.log("productos>>>: ", productos);
-    console.log("tipos de producto>>>: ", tiposProducto);    
+     
     const calcularTotalesPorCategoria = () => {
         const totalesPorCategoria = {};
 
@@ -86,10 +105,12 @@ const RepIngresos = () => {
     };
 
     return (
-        <div>
-            <h2>Reporte de Ingresos</h2>
+        
+        <div className='p-4'>
+            <Toast ref={toast} />
+            <h2 className='text-2xl font-bold mb-4'>Reporte de Ingresos</h2>
 
-            <div className="p-grid">
+            <div className="flex gap-4 mb-4">
                 <div className="p-col-12 p-md-4">
                     <Calendar value={fechaInicial} onChange={(e) => setFechaInicial(e.value)} placeholder="Fecha Inicial" showIcon />
                 </div>
@@ -97,17 +118,16 @@ const RepIngresos = () => {
                     <Calendar value={fechaFinal} onChange={(e) => setFechaFinal(e.value)} placeholder="Fecha Final" showIcon />
                 </div>
                 <div className="p-col-12 p-md-4">
-                    <Button label="Generar Reporte" icon="pi pi-check" onClick={fetchIngresos} />
+                    <Button label="Generar Reporte" icon="pi pi-print" onClick={fetchIngresos} />
                 </div>
             </div>
-
-            <DataTable value={ingresos} footer={renderFooter()}>
+            <DataTable value={ingresos} footer={renderFooter()} >
                 <Column field="fechaIngreso" header="Fecha de Ingreso" />
                 <Column field="cantIngreso" header="Cantidad Ingresada" />
                 <Column
                     field="idProd_fk"
                     header="Producto"
-                    body={(rowData) => productos[rowData.idProd_fk]?.nombreProducto}
+                    body={(rowData) => productos[rowData.idProd_fk]?.nombProd}
                 />
                 <Column
                     field="idProd_fk"
